4cd68ae2973234456d59b82ca72aad21b7162933
eyelidlessness <gnosis@gmail.com>
2017-04-28 19:01:31 -0700

    Break out Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import Header from './Header';
 import './App.css';
 
 
 const App = () => {
   return (
     <div className="App">
-      <div className="App-header">
-        <h2>Welcome to React</h2>
-      </div>
+      <Header />
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
